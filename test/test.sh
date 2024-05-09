60d93b941b66ab21a466ff855239bace46c1b4d7
Meriç Bağlayan <baglayan19@itu.edu.tr>
2024-05-09 21:23:01 +0300

    Add test files

---
+++ test/test.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+[ -e ./calico_log.txt ] && rm ./calico_log.txt
+[ -e ./valgrind_log.txt ] && rm ./valgrind_log.txt
+
+[ -e ../src/main ] && rm ../src/main
+
+calico test.yaml |& tee calico_log.txt
+
+valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all ../src/main 1 |& tee -a valgrind_log.txt
+valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all ../src/main 2 |& tee -a valgrind_log.txt
+valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all ../src/main 3 |& tee -a valgrind_log.txt
