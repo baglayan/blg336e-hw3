03c9258d2c4c8b9e9ea214ef528292ed0fa3cd3c
Meriç Bağlayan <baglayan19@itu.edu.tr>
2024-05-15 15:36:26 +0300

    Navigate to src directory before running calico

--- test/test.sh
+++ test/test.sh
@@ -4,9 +4,9 @@
 
 [ -e ../src/main ] && rm ../src/main
 
-calico ../src/test.yaml |& tee calico_log.txt
-
 cd ../src
+calico test.yaml |& tee ../test/calico_log.txt
+
 
 [ -e ./main ] && valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all ./main 1 |& tee -a ../test/valgrind_log.txt
 [ -e ./main ] && valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all ./main 2 |& tee -a ../test/valgrind_log.txt
